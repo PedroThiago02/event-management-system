3ce3e5a3cf9a5a6b96dfcd84d54daa80d1f070d0
Pedro Thiago <pedrothiagom456@gmail.com>
2026-05-12 14:42:46 -0300

    feat: create Category entity

---
+++ src/main/java/com/devsuperior/sistemaevento/entities/Category.java
@@ -0,0 +1,54 @@
+package com.devsuperior.sistemaevento.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_category")
+public class Category implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    public Category() {
+    }
+
+    public Category(Long id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
